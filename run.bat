b60b0d3db7ad49d9812d7970098377fdb9ac7bdf
灿飞 <canfeit@qq.com>
2017-10-19 11:24:28 +0800

    chore

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ cd %~dp0app
 call yarn&&start /min run
 cd ..
 pip install -r api/requirements.txt --upgrade
-pip install sphinx_rtd_theme
+pip install git+https://github.com/sphinx-doc/sphinx sphinx_rtd_theme
 api/run
 cd %startdir%
