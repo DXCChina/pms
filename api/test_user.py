9d1abf3a7d91826f9e499b2fc1490d2656a45d18
灿飞 <canfeit@qq.com>
2017-11-09 16:15:46 +0800

    add rest 接口测试

--- api/test_user.py
+++ api/test_user.py
@@ -4,6 +4,7 @@ import random
 import string
 from app import app
 from model import user
+from flask import json
 
 
 def test_user_model():
@@ -27,11 +28,26 @@ def test_user_model():
     user.update({'username': username, 'email': email, 'id': id})
 
     assert user.findOneById(id)['password'] == password
-    
+
     assert user.findOneByEmail(email)['email'] == email
 
 
-# def test_get_user():
-#     tester = app.test_client()
-#     response = tester.get('/api/user')
-#     assert response.data == b'{\n  "msg": "Missing JWT in headers and cookies"\n}\n'
+tester = app.test_client()
+
+
+def test_get_user():
+    response = tester.get('/api/user')
+    assert response.data == b'{\n  "msg": "Missing JWT in headers and cookies"\n}\n'
+
+
+def test_login():
+    response = tester.post(
+        '/api/login',
+        data=json.dumps({
+            'username': 'aaa@aaa.qaaa',
+            'password': 'aaa@aaa.qaaa'
+        }),
+        content_type='application/json')
+    data = json.loads(response.data)
+
+    assert data['access_token']
