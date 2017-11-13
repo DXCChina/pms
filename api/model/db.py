a8c6dcb81c9e38725f25ac462dcef3b8c6c24e5e
灿飞 <canfeit@qq.com>
2017-11-13 13:40:44 +0800

    chore

--- api/model/db.py
+++ api/model/db.py
@@ -13,6 +13,15 @@ PY_DB_NAME = 'PY_DB_NAME' in environ and environ['PY_DB_NAME'] or 'pms'
 PY_DB_HOST = 'PY_DB_HOST' in environ and environ['PY_DB_HOST'] or 'localhost'
 PY_DB_DEBUG = 'PY_ENV' in environ and environ['PY_ENV'] == 'dev'
 
+print('建立数据库连接:', {
+    'host': PY_DB_HOST,
+    'user': PY_DB_USERNAME,
+    'password': PY_DB_PASSWORD,
+    'db': PY_DB_NAME,
+    'debug': PY_DB_DEBUG,
+    'cursorclass': pymysql.cursors.DictCursor,
+    'charset':'utf8'
+})
 # pylint:disable=c0103
 db = pymysql.connect(
     host=PY_DB_HOST,
@@ -21,12 +30,4 @@ db = pymysql.connect(
     db=PY_DB_NAME,
     cursorclass=pymysql.cursors.DictCursor,
     charset='utf8')
-print('建立数据库连接:', {
-    'host': PY_DB_HOST,
-    'user': PY_DB_USERNAME,
-    'password': PY_DB_PASSWORD,
-    'db': PY_DB_NAME,
-    'debug': PY_DB_DEBUG,
-    'cursorclass': pymysql.cursors.DictCursor
-})
 
