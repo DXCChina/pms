fa407918e7880c99702591cde5140e3c892d07c1
LG <GL717@outlook.com>
2017-11-23 10:42:48 +0800

    添加需求详情modal

--- app/src/app/pages/pm/pm-dashboard.component/pm-dashboard.component.ts
+++ app/src/app/pages/pm/pm-dashboard.component/pm-dashboard.component.ts
@@ -1,8 +1,39 @@
 import {Component} from "@angular/core";
+import {DemandDetailDialogComponent} from "../../../theme/components/demand-detail-dialog/demand-detail-dialog.component";
+import {TaskDetailDialogComponent} from "../../../theme/components/task-detail-dialog/task-detail-dialog.component";
+
+import {MatDialog} from "@angular/material";
 
 @Component({
   selector: 'pm-dashboard',
   templateUrl: './pm-dashboard.component.html',
   styleUrls: ['./pm-dashboard.component.scss']
 })
-export class PmDashboardComponent { }
+export class PmDashboardComponent {
+  constructor(public dialog: MatDialog){
+
+  }
+  initDemand(){
+    let dialogRef = this.dialog.open(DemandDetailDialogComponent, {
+      width: '750px',
+      height:'70vh',
+      data: { name: 'dd', animal: 'dd' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  initActivity(){
+    let dialogRef = this.dialog.open(TaskDetailDialogComponent, {
+      width: '750px',
+      height:'70vh',
+      data: { name: 'dd', animal: 'dd' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+}
