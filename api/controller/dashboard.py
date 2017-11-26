c56c28b5274844c6651c99c5ef5f1f7e1c30d9ee
Bilibala <wangqianxiang@outlook.com>
2017-11-27 02:07:54 +0800

    update: dashboard接口代码

--- api/controller/dashboard.py
+++ api/controller/dashboard.py
@@ -3,15 +3,10 @@
 
 @author: Wang Qianxiang
 '''
-from flask_jwt_extended import (create_access_token, get_jwt_identity,
-                                get_jwt_claims, fresh_jwt_required,
-                                set_access_cookies, unset_jwt_cookies)
-from marshmallow import Schema, fields
-from flask import jsonify, request, abort, Blueprint, session
-from rbac.context import PermissionDenied
+from flask_jwt_extended import (get_jwt_identity, fresh_jwt_required)
+from flask import jsonify
 
 from model import dashboard
-# app = Blueprint('user', __name__, url_prefix='/api')  # pylint: disable=c0103
 
 
 @fresh_jwt_required
@@ -20,7 +15,13 @@ def project_demand(project_id):
 
     GET /api/dashboard/<int:project_id>/demand
     '''
-    return jsonify(dashboard.findRoleByMId(project_id, get_jwt_identity()))
+    if dashboard.find_owner(project_id) == get_jwt_identity():
+        res = dashboard.find_all_demand(project_id)
+    elif dashboard.find_role(project_id, get_jwt_identity()).role:
+        res = dashboard.find_demand(project_id, get_jwt_identity())
+    else:
+        return jsonify({"msg": "No permission!"}), 403
+    return jsonify(res)
 
 
 @fresh_jwt_required
@@ -29,9 +30,13 @@ def project_activity(project_id):
 
     GET /api/dashboard/<int:project_id>/activity
     '''
-    print(get_jwt_identity())
-    print(get_jwt_claims())
-    return jsonify(dashboard.findRoleByMId(project_id, get_jwt_identity()))
+    if dashboard.find_owner(project_id) == get_jwt_identity():
+        res = dashboard.find_all_activity(project_id)
+    elif dashboard.find_role(project_id, get_jwt_identity()):
+        res = dashboard.find_activity(project_id, get_jwt_identity())
+    else:
+        return jsonify({"msg": "No permission!"}), 403
+    return jsonify(res)
 
 
 @fresh_jwt_required
@@ -40,7 +45,13 @@ def project_test_case(project_id):
 
     GET /api/dashboard/<int:project_id>/case
     '''
-    return jsonify(dashboard.findRoleByMId(project_id, get_jwt_identity()))
+    if dashboard.find_owner(project_id) == get_jwt_identity():
+        res = dashboard.find_all_test_case(project_id)
+    elif dashboard.find_role(project_id, get_jwt_identity()):
+        res = dashboard.find_test_case(project_id, get_jwt_identity())
+    else:
+        return jsonify({"msg": "No permission!"}), 403
+    return jsonify(res)
 
 
 @fresh_jwt_required
@@ -49,4 +60,12 @@ def project_test_result(project_id):
 
     GET /api/dashboard/<int:project_id>/result
     '''
-    return jsonify(dashboard.findRoleByMId(project_id, get_jwt_identity()))
+    if dashboard.find_owner(project_id) == get_jwt_identity():
+        res = dashboard.find_all_test_result(project_id)
+    elif dashboard.find_role(project_id, get_jwt_identity()).role == 'dev':
+        res = dashboard.find_test_result_for_dev(project_id, get_jwt_identity())
+    elif dashboard.find_role(project_id, get_jwt_identity()).role == 'test':
+        res = dashboard.find_test_result_for_test(project_id, get_jwt_identity())
+    else:
+        return jsonify({"msg": "No permission!"}), 403
+    return jsonify(res)
--- api/model/dashboard.py
+++ api/model/dashboard.py
@@ -4,49 +4,59 @@
 @author: Wang Qianxiang
 '''
 
-from .db import (Demand, Activity, TestCase, TestResult, ProjectMember)
+from .db import (Demand, Activity, TestCase, TestResult, Project, ProjectMember)
 
 
-def findRoleByMId(pId, mId):
+def find_owner(p_id):
+    '''查询项目拥有者'''
+    return Project.getOne(Project.id == p_id).ownerId
+
+
+def find_role(p_id, m_id):
     '''查询项目成员权限'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+    return ProjectMember.getOne(ProjectMember.projectId == p_id and ProjectMember.memberId == m_id)
 
 
-def findDemandByMId(pId, mId):
-    '''按MId查询相关项目需求'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_demand(p_id, m_id):
+    '''按M_id查询相关项目需求'''
+    return Demand.get(Demand.projectId == p_id and Demand.ownerId == m_id)
 
 
-def findActivityByMId(pId, mId):
-    '''按MId查询相关项目活动'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_activity(p_id, m_id):
+    '''按M_id查询相关项目活动'''
+    return Activity.get(Activity.projectId == p_id and Activity.memberId == m_id)
 
 
-def findTestCaseByMId(pId, mId):
-    '''按MId查询相关项目测试案例'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_test_case(p_id, m_id):
+    '''按M_id查询相关项目测试案例'''
+    return TestCase.get(TestCase.projectId == p_id and TestCase.ownerId == m_id)
 
 
-def findTestResultByMId(pId, mId):
-    '''按MId查询相关项目测试结果'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_test_result_for_dev(p_id, m_id):
+    '''按M_id查询开发相关项目测试结果'''
+    return TestResult.get(TestResult.projectId == p_id and TestResult.devId == m_id)
 
 
-def findAllDemand(pId):
-    '''按MId查询全部项目需求'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_test_result_for_test(p_id, m_id):
+    '''按M_id查询测试相关项目测试结果'''
+    return TestResult.get(TestResult.projectId == p_id and TestResult.ownerId == m_id)
 
 
-def findAllActivity(pId):
-    '''按MId查询全部项目需求'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_all_demand(p_id):
+    '''按M_id查询全部项目需求'''
+    return Demand.getOne(Demand.projectId == p_id)
 
 
-def findAllTestCase(pId):
-    '''按MId查询全部项目需求'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_all_activity(p_id):
+    '''按M_id查询全部项目活动'''
+    return Activity.getOne(Activity.projectId == p_id)
 
 
-def findAllTestResult(pId):
-    '''按MId查询全部项目需求'''
-    return ProjectMember.getOne(ProjectMember.projectId == pId and ProjectMember.memberId == mId)
+def find_all_test_case(p_id):
+    '''按M_id查询全部项目测试案例'''
+    return TestCase.getOne(TestCase.projectId == p_id)
+
+
+def find_all_test_result(p_id):
+    '''按M_id查询全部项目测试结果'''
+    return TestResult.getOne(TestResult.projectId == p_id)
