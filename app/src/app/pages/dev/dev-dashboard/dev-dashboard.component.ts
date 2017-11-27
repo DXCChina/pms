9d58571198bc8e3f288d142208258576af88daa0
LG <GL717@outlook.com>
2017-11-27 23:00:09 +0800

    achieve review detail in dev role

--- app/src/app/pages/dev/dev-dashboard/dev-dashboard.component.ts
+++ app/src/app/pages/dev/dev-dashboard/dev-dashboard.component.ts
@@ -1,8 +1,28 @@
 import {Component} from "@angular/core";
+import {DevDemandDetailService} from "./dev-dashboard.service";
+import {MatDialog} from "@angular/material";
+import {DemandDetailModalComponent} from "../demand-detail-modal/demand-detail-modal.component";
 
 @Component({
   selector: 'dev-dashboard',
   templateUrl: './dev-dashboard.component.html',
-  styleUrls: ['./dev-dashboard.component.scss']
+  styleUrls: ['./dev-dashboard.component.scss'],
+  providers:[DevDemandDetailService]
 })
-export class DevDashboardComponent {}
+export class DevDashboardComponent {
+  constructor(private _service:DevDemandDetailService, private dialog:MatDialog){}
+
+  reviewDemandDetail(id){
+    this._service.reviewDemandDetail(id)
+      .then(res => {
+        let dialogRef = this.dialog.open(DemandDetailModalComponent, {
+          width: '750px',
+          height:'61vh',
+          data: res.data
+        });
+        dialogRef.afterClosed().subscribe(result => {
+          console.log('The dialog was closed');
+        });
+      });
+  }
+}
