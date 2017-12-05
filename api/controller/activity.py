0ce08c8cdcd49af2bc6e9fd5e25da2bbec39b62e
灿飞 <canfeit@qq.com>
2017-12-05 10:25:35 +0800

    权限验证

--- api/controller/activity.py
+++ api/controller/activity.py
@@ -42,7 +42,7 @@ def demand_activity_done(activity_id, data):
 
 
 @fresh_jwt_required
-@identity.check_permission("create", 'activity')
+@identity.check_permission("create", 'task')
 def activity_add():
     '''创建项目活动'''
     data = request.json
@@ -66,7 +66,7 @@ def activity_add():
 
 
 @fresh_jwt_required
-@identity.check_permission("update", 'activity')
+@identity.check_permission("update", 'task')
 def activity_update():
     '''更新项目活动'''
     data = request.json
