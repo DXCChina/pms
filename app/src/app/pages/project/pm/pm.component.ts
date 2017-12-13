0354905b8cb0aef1f11ccd39e1d1437104e19fb0
wangqx <qianxiang.wang@hpe.com>
2017-12-13 19:14:07 +0800

    fix: project侧边导航代码位置等

--- app/src/app/pages/project/pm/pm.component.ts
+++ app/src/app/pages/project/pm/pm.component.ts
@@ -1,10 +1,7 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm',
-  styleUrls: ['./pm.component.scss'],
-  template: `
-    <pms-sidebar></pms-sidebar>
-    <div class="pms-main"><router-outlet></router-outlet></div>`
+  selector: 'app-pm',
+  template: `<router-outlet></router-outlet>`
 })
 export class PmComponent { }
--- app/src/app/pages/project/project.component.ts
+++ app/src/app/pages/project/project.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pages',
+  selector: 'app-project',
   template: `
-        <router-outlet></router-outlet>
-    `
+    <pms-sidebar></pms-sidebar>
+    <div class="pms-main"><router-outlet></router-outlet></div>
+  `
 })
-export class Project {
-  constructor() {
-  }
-}
+export class ProjectComponent { }
