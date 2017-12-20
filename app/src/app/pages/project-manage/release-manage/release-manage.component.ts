a1ba8daa49502b85f96d180e6303a64969a16689
Dong Xing <xingdong.com@live.com>
2017-12-20 16:26:30 +0800

    add timeLine component

--- app/src/app/pages/project-manage/release-manage/release-manage.component.ts
+++ app/src/app/pages/project-manage/release-manage/release-manage.component.ts
@@ -1,36 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {TimelineElement} from "../../../theme/components/horizontal-timeline/timeline-element";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ReleaseManageService} from "./release-manage.service";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-release-manage',
   templateUrl: './release-manage.component.html',
-  styleUrls: ['./release-manage.component.css']
+  styleUrls: ['./release-manage.component.css'],
+  providers: [ReleaseManageService]
 })
 export class ReleaseManageComponent implements OnInit {
 
-  timeline: TimelineElement[];
+  form: FormGroup;
+  timeLine: TimelineElement[];
   content: string;
+  title: string;
   isExpanded: boolean;
-  constructor(private router:Router) {
+  constructor(private router: Router, private service: ReleaseManageService) {
+
+    this.content = '';
+    this.title = '';
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      content: new FormControl('', [Validators.required])
+    });
+
     this.isExpanded = false;
-    this.content = 'Lorme 111';
-    this.timeline = [
-      { caption: '16 Jan', date: new Date(2014, 1, 16), selected: true, title: 'Horizontal Timeline', content: this.content },
-      { caption: '28 Feb', date: new Date(2014, 2, 28), title: 'Event title here', content: this.content },
-      { caption: '20 Mar', date: new Date(2014, 3, 20), title: 'Event title here', content: this.content },
-      { caption: '20 May', date: new Date(2014, 5, 20), title: 'Event title here', content: this.content },
-      { caption: '09 Jul', date: new Date(2014, 7, 9), title: 'Event title here', content: this.content },
-      { caption: '30 Aug', date: new Date(2014, 8, 30), title: 'Event title here', content: this.content },
-      { caption: '15 Sep', date: new Date(2014, 9, 15), title: 'Event title here', content: this.content },
-      { caption: '01 Nov', date: new Date(2014, 11, 1), title: 'Event title here', content: this.content },
-      { caption: '10 Dec', date: new Date(2014, 12, 10), title: 'Event title here', content: this.content },
-      { caption: '29 Jan', date: new Date(2015, 1, 19), title: 'Event title here', content: this.content },
-      { caption: '3 Mar', date: new Date(2015, 3, 3), title: 'Event title here', content: this.content },
-    ]
+
+    this.timeLine = [
+        {caption: "Mon Dec 18", content: "release content", date: new Date(2014, 1, 16), title: "release1", selected: true},
+        {caption: "Mon Dec 18", content: "release content", date: new Date(2014, 1, 17), title: "release2"},
+        {caption: "Mon Dec 18", content: "release content", date: new Date(2014, 1, 18), title: "release3"},
+        {caption: "Mon Dec 18", content: "release 4 content", date: new Date(2014, 9, 19), title: "release 4"},
+        {caption: "Mon Dec 18", content: "release 5 content", date: new Date(2015, 1, 20), title: "release 5"}
+      ]
   }
 
   ngOnInit() {
+    this.getReleaseList();
+    console.log(new Date().getMonth(), new Date().getFullYear(), new Date().getDate())
+  }
+
+  getReleaseList() {
+    this.service.getReleaseList()
+      .then(res => {
+        if (res.message === 'ok') {
+          // this.timeLine = res.data;
+          // this.timeLine.map(item => {
+          //   return item.date = new Date(this.translateDate(item.date, 'YYYY'), this.translateDate(item.date, 'M'), this.translateDate(item.date, 'DD'))}
+          //   );
+          // this.timeLine[0].selected = true;
+          // console.log(this.timeLine);
+          // console.log(moment(this.timeLine[0].date).format('YYYY'));
+          // console.log(moment(this.timeLine[0].date).format('DD'));
+          // console.log(moment(this.timeLine[0].date).format('M'));
+        }
+      }, err => console.log(err));
+  }
+
+  create(form: any) {
+    this.isExpanded = !this.isExpanded;
+    this.service.createRelease(form.title, form.content, moment().format("MMM DD"))
+      .then(res => {
+        if (res.message === 'ok') {
+          this.title = '';
+          this.content = '';
+        }
+      }, err => console.log(err));
   }
 
   goToDashboard(item: any){
@@ -39,5 +77,9 @@ export class ReleaseManageComponent implements OnInit {
 
   setExpanded() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  translateDate(date: Date, format:string) {
+    return Number(moment(date).format(format));
   }
 }
---
+++ app/src/app/pages/project-manage/release-manage/release-manage.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {GlobalState} from "../../../global.state";
+
+@Injectable()
+export class ReleaseManageService {
+  createReleaseUrl: string;
+  getReleaseListUrl: string;
+  constructor(private http: Http, private Global: GlobalState) {
+    this.createReleaseUrl = '/api/release';
+    this.getReleaseListUrl = '/api/release';
+  }
+
+  createRelease(title: string, content: string, caption: any): Promise<any> {
+    const projectId = sessionStorage.getItem('projectId');
+    const body = {title, content, caption, projectId};
+    return this.http.post(this.createReleaseUrl, body)
+      .toPromise()
+      .then(this.Global.extractData)
+      .catch(this.Global.handleError);
+  }
+
+  getReleaseList(): Promise<any> {
+    const params = {'projectId': sessionStorage.getItem('projectId')};
+    return this.http.get(this.getReleaseListUrl, {params})
+      .toPromise()
+      .then(this.Global.extractData)
+      .catch(this.Global.handleError);
+  }
+}
