f4e474be5480fa65c456e1746ac2737153ebe05a
Dong Xing <xingdong.com@live.com>
2017-12-21 12:43:37 +0800

    overwrite timeLine component

---
+++ app/src/app/theme/components/timeLine/timeLine.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'time-line',
+  templateUrl: 'timeLine.component.html',
+  styleUrls: ['timeLine.component.scss']
+})
+export class TimeLineComponent {
+
+}
