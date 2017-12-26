487d44c5823ee5b9f0d387fe982eaa53760ad068
Dong Xing <xingdong.com@live.com>
2017-12-26 18:30:10 +0800

    angular update to 5.12 && add font-awesome,angular-froala-wysiwyg editor

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ set path=%PYTHON_HOME%%path%
 call cnpm i||call yarn||call npm i
 start /min run
 cd ..
-set PYTHON_HOME=C:\Users\wajianhu\AppData\Local\Programs\Python\Python36\Scripts\;C:\Users\wajianhu\AppData\Local\Programs\Python\Python36\;
+set PYTHON_HOME=C:\Users\xingd\AppData\Local\Programs\Python\Python36\Scripts\;C:\Users\xingd\AppData\Local\Programs\Python\Python36\;
 set path=%PYTHON_HOME%%path%
 chcp 65001
 pip install -r api/requirements.txt --upgrade
